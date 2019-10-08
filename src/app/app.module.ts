3228005d5a0de582bca21430d54e83cc01d9b72b
MFCharbo <mathieu.charbonneau12@gmail.com>
2019-10-08 08:35:25 -0400

    Angular/Auth0 functional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CallbackComponent } from './callback/callback.component';
+import { ListsComponent } from './lists/lists.component';
+
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CallbackComponent,
+    ListsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
